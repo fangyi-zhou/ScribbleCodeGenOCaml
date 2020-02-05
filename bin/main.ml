a652064327d9f716a6dbaf25bdeb600d8ab14d70
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2020-02-05 15:25:40 +0000

    Argument parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,57 @@
-let () = print_endline "Hello, World!"
+open! Stdio
+open ScribbleCodeGen
+
+let usage =
+  "\nUSAGE: " ^ Sys.argv.(0)
+  ^ " [--help] --protocol <string> --role <string> [--mode \
+     <legacyapi|eventapi|fstar>] [--output <string>] [--recursion] <string>\n\n\
+     FILENAME:\n\n\
+    \    <string>              Path to Scribble Output\n\n\
+     OPTIONS:\n\n\
+    \    --protocol <string>   Name of Scribble Protocol\n\
+    \    --role <string>       Name of Local Role in the Protocol\n\
+    \    --mode <legacyapi|eventapi|fstar>\n\
+    \                          Mode of Code Generation, default F# Event \
+     Style\n\
+    \    --output, -o <string> Path to Output Filename\n\
+    \    --recursion           Allow Refinements on Recursion (Scribble \
+     dev-assrt)\n\
+    \    --help                display this list of options.\n"
+
+let protocol = ref ""
+
+let role = ref ""
+
+let mode = ref Types.FStar
+
+let recursion = ref true
+
+let err msg = print_string msg ; print_string usage ; exit 1
+
+let handle_mode = function
+  | "legacyapi" -> mode := Types.LegacyApi
+  | "eventapi" -> mode := Types.EventApi
+  | "fstar" -> mode := Types.FStar
+  | _ -> err "Error: Unknown mode\n"
+
+let speclist =
+  [ ("--protocol", Arg.Set_string protocol, "Name of Scribble Protocol")
+  ; ("--role", Arg.Set_string role, "Name of Local Role in the Protocol")
+  ; ("--mode", Arg.String handle_mode, "Mode of Code Generation")
+  ; ( "--output"
+    , Arg.Set_string CodePrinter.fileName
+    , "Path to Output Filename" )
+  ; ("-o", Arg.Set_string CodePrinter.fileName, "Path to Output Filename")
+  ; ("--recursion", Arg.Set recursion, "Allow Refinements on Recursion") ]
+
+let () =
+  let run filename =
+    if !protocol = "" then err "Error: Protocol not set" ;
+    if !role = "" then err "Error: Role not set" ;
+    if filename = "" then err "Error: File not set" ;
+    let file = In_channel.create filename in
+    let content = In_channel.input_all file in
+    In_channel.close file ;
+    Lib.processScribbleOutput content !protocol !role !mode !recursion
+  in
+  Arg.parse speclist run usage
